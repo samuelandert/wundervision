4caeefb111fb5b933e6cb7993c9a3ba9f8f7707d
Samuel Andert <samuel@ora.earth>
2023-09-29 15:23:53 +0200

    vite fix deploy test

--- apps/slube/vite.config.ts
+++ apps/slube/vite.config.ts
@@ -2,7 +2,6 @@ import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import autoImport from 'composer-import';
-import dynamicImport from 'vite-plugin-dynamic-import-polyfill';
 
 export default defineConfig({
 	plugins: [
@@ -21,7 +20,6 @@ export default defineConfig({
 		}),
 		sveltekit(),
 		purgeCss(),
-		dynamicImport(),
 	],
 	build: {
 		rollupOptions: {
