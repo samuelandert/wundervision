691957ce5d36cada2b979bfdbd399614f1beda32
Samuel Andert <samuel@ora.earth>
2023-09-29 17:00:53 +0200

    test deploy

--- apps/slube/src/routes/+page.svelte
+++ apps/slube/src/routes/+page.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
 	// import type { PageData } from './$types';
 	export let data: PageData;
+	import { composer } from '$lib/composables/flow';
+
+	let Component;
+	let componentName = 'Composer';
+
+	onMount(async () => {
+		const components = import.meta.glob('/src/lib/components/*.svelte');
+
+		for (const path in components) {
+			if (path.endsWith(`${componentName}.svelte`)) {
+				Component = (await components[path]()).default;
+				break;
+			}
+		}
+	});
 </script>
 
-<Header />
-<SubHeader />
+<!-- <Header /> -->
+<svelte:component this={Component} {composer} />
+<!-- <SubHeader /> -->
 
 <div class="container p-12 mx-auto flex justify-center items-center">
 	<div class="space-y-6 text-center flex flex-col items-center">
