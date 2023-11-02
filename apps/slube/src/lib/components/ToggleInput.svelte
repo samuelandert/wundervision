f6aab4529902f210ebc3f66d2f97a31deeb13614
Samuel Andert <samuel@andert.me>
2023-11-02 20:34:52 +0100

    removed slube

--- apps/slube/src/lib/components/ToggleInput.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { SlideToggle } from '@skeletonlabs/skeleton';
-
-	export let childInput;
-
-	const { form, field, constraints } = childInput;
-</script>
-
-<SlideToggle name={field.name} bind:checked={$form[field.name]} {...constraints[field.name]} />
